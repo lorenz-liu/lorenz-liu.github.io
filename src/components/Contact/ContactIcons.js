f792ae3f0e1e5dc39b390e7391e86bf6d50047ca
刘兆薰_19373345 <lau@buaa.edu.cn>
2023-04-23 16:47:19 +0800

    update

--- src/components/Contact/ContactIcons.js
+++ src/components/Contact/ContactIcons.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { Link } from 'react-router-dom';
 import data from '../../data/contact';
 
 const ContactIcons = () => (
   <ul className="icons">
     {data.map((s) => (
       <li key={s.label}>
-        <a href={s.link} target={s.label === 'WeChat' ? '_self' : '_blank'} rel="noopener noreferrer">
-          <FontAwesomeIcon icon={s.icon} />
-        </a>
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+        {s.label !== 'WeChat' ? <a href={s.link} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={s.icon} /></a> : <Link to="/contact"><FontAwesomeIcon icon={s.icon} /></Link>}
       </li>
     ))}
   </ul>
