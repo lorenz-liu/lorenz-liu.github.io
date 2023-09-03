6990576493504a93aec09add8756507a4abb7356
LorenzL <zhaoxun.liu@proton.me>
2023-09-03 15:13:29 +0800

    comment src/components/Stats/Site.js and src/data/stats/site.js

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -1,16 +1,16 @@
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
 
 /* Keys match keys returned by the github api. Fields without keys are
  * mostly jokes. To see everything returned by the github api, run:
  curl https://api.github.com/repos/GitLorenzL/GitLorenzL.github.io
  */
 const data = [
-  {
-    label: 'Last updated at',
-    key: 'pushed_at',
-    link: 'https://github.com/GitLorenzL/GitLorenzL.github.io/commits',
-    format: (x) => dayjs(x).format('MMMM DD, YYYY'),
-  },
+  // {
+  //   label: 'Last updated at',
+  //   key: 'pushed_at',
+  //   link: 'https://github.com/GitLorenzL/GitLorenzL.github.io/commits',
+  //   format: (x) => dayjs(x).format('MMMM DD, YYYY'),
+  // },
 ];
 
 export default data;
