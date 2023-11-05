db37ce2924fe23815c297ddbf7ddf28cb0c5c251
LorenzL <zhaoxun.liu@proton.me>
2023-11-05 13:44:01 -0500

    update

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,15 @@
 const degrees = [
   {
+    school: 'University of Toronto',
+    degree: 'Master of Science in Applied Computing (On-going)',
+    link: 'https://mscac.utoronto.ca/',
+    year: 2023,
+  },
+  {
     school: 'Beihang University',
     degree: 'B.Eng. in Computer Science and Technology',
     link: 'https://buaa.edu.cn',
     year: 2019,
-  },
-  {
-    school: 'University of Toronto',
-    degree: 'Master of Science in Applied Computing (On-going)',
-    link: 'https://www.utoronto.ca',
-    year: 2023,
   },
 ];
 
--- src/pages/BlogDetail.js
+++ src/pages/BlogDetail.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Markdown from 'markdown-to-jsx';
 
-const BlogDetail = () => (
-  <div>
-    TO BE CONSTRUCTED.
-    <a href="/blogs">GO BACK</a>
-  </div>
-);
+import Main from '../layouts/Main';
+
+const BlogDetail = () => {
+  const { blog } = useParams();
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    import(`../data/blogs/${blog}`)
+      .then((res) => {
+        fetch(res.default)
+          .then((r) => r.text())
+          .then(setMarkdown);
+      });
+  });
+
+  const count = markdown.split(/\s+/)
+    .map((s) => s.replace(/\W/g, ''))
+    .filter((s) => s.length).length;
+
+  return (
+    <Main
+      title="About"
+      description='Learn about Zhaoxun "Lorenz" Liu'
+    >
+      <article className="post markdown" id="about">
+        <header>
+          <div className="title">
+            <a href="/blogs">GO BACK</a>
+            <h2><Link to="/about">BlogDetail (TO BE COMPLETED)</Link></h2>
+            <p>(in about {count} words)</p>
+          </div>
+        </header>
+        <Markdown>
+          {markdown}
+        </Markdown>
+      </article>
+    </Main>
+  );
+};
 
 export default BlogDetail;
