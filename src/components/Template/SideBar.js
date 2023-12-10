c71ae07430c00e5eb9a75e03a48ff4636ed37e95
LorenzL <zhaoxun.liu@proton.me>
2023-12-10 16:50:13 -0500

    update map

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -34,6 +34,14 @@ const SideBar = () => (
       </ul>
     </section>
 
+    <div className="stat">
+      <script
+        type="text/javascript"
+        id="clustrmaps"
+        src="//cdn.clustrmaps.com/map_v2.js?cl=230505&w=450&t=tt&d=HfwB-ysPDBgnoPg89MNPfnXkiJm3BPHh37SYx1zeuw8&co=f4f4f4&ct=d1d1d1&cmo=b6c7f2&cmn=ffa500"
+      />
+    </div>
+
     <section id="footer">
       <ContactIcons />
       <p className="copyright">&copy; Zhaoxun &quot;Lorenz&quot; Liu.</p>
