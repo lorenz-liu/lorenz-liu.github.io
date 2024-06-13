158e68f0cb6db3673b0dacb83760738e328e6457
Zhaoxun "Lorenz" Liu <lorenz@cs.toronto.edu>
2024-06-13 13:28:23 -0400

    update

--- src/data/cv/publications.js
+++ src/data/cv/publications.js
@@ -1,20 +1,20 @@
 const publications = [
   {
+    title: 'CrossKeys: Text Entry for Virtual Reality Using a Single Controller via Wrist Rotation',
+    authors: 'Zhaoxun Liu*, Xiaolong Liu, Lili Wang',
+    url: 'https://doi.org/10.1080/10447318.2024.2358456',
+    pub: 'International Journal of Human–Computer Interaction (IJHCI), 2024',
+  },
+  {
     title: 'Hands-Free Is Fine: Gaze-Dominant Object Manipulation in Virtual Reality',
     authors: 'Zhaoxun Liu*, Xiaolong Liu, Lili Wang',
     url: '/files/hands-free-is-fine.pdf',
-    pub: 'IEEE International Symposium on Mixed and Augmented Reality (ISMAR), 2023',
-  },
-  {
-    title: 'CrossKeys: Text Entry for Virtual Reality Using a Single Controller via Wrist Rotation',
-    authors: 'Zhaoxun Liu*, Xiaolong Liu, Lili Wang',
-    url: '/files/crosskeys.pdf',
-    pub: 'IEEE Conference on Virtual Reality and 3D User Interfaces (VR), 2023',
+    pub: 'Journal of Beihang University, 2023 (Undergraduate Thesis)',
   },
   {
     title: 'Temporal Transformer Networks with Self-Supervision for Action Recognition',
     authors: 'Yongkang Zhang, Jun Li, Na Jiang, Guoming Wu, Han Zhang, Zhiping Shi, Zhaoxun Liu*, Zizhang Wu',
-    url: 'https://ieeexplore.ieee.org/document/10073948',
+    url: 'https://doi.org/10.1109/JIOT.2023.3257992',
     pub: 'IEEE Internet of Things Journal (IoT), 2023',
   },
 ];
